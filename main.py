3c6835ae8d16287d367ef3864f885c778685926f
Gijs Tempelman <gijstempelman@gmail.com>
2023-02-14 11:26:45 +0000

    Complete code and add review_words data file.

--- main.py
+++ main.py
@@ -3,6 +3,55 @@ from tkinter import *
 import pandas as pd
 import random
 
+# Create an empty dictionary to hold the current flashcard and a dictionary to hold the flashcards to review
+current_flashcard = {}
+review = {}
+
+# Try to read the flashcards from a review file; if the file is not found, load the flashcards from a data file
+try:
+    data = pd.read_csv("data/review_words.csv", encoding="latin1")
+except FileNotFoundError:
+    word_data = pd.read_csv("data/icelandic_words.csv", encoding="latin1")
+    print(word_data)
+    review = word_data.to_dict(orient="records")
+else:
+    review = data.to_dict(orient="records")
+
+# ---------- Functions ---------- #
+
+# Function to display the next flashcard
+def next_flashcard():
+    global current_flashcard, card_view_time
+    # Cancel the current flashcard display
+    window.after_cancel(card_view_time)
+    # Choose a new random flashcard and display it on the front of the card
+    current_flashcard = random.choice(review)
+    canvas.itemconfig(flashcard_title, text="Icelandic", fill="black")
+    canvas.itemconfig(flashcard_word, text=current_flashcard["Icelandic"], fill="black")
+    canvas.itemconfig(flashcard_background, image=front_image)
+    # Set a timer to flip the flashcard after 4.5 seconds
+    card_view_time = window.after(4500, func=flip_flashcard)
+
+
+# Function to flip the flashcard and display the English word on the back of the card
+def flip_flashcard():
+    canvas.itemconfig(flashcard_title, text="English", fill="white")
+    canvas.itemconfig(flashcard_word, text=current_flashcard["English"], fill="white")
+    canvas.itemconfig(flashcard_background, image=back_image)
+
+
+# Function to mark the current flashcard as correct and remove it from the review list
+def correct_flashcard():
+    review.remove(current_flashcard)
+    print(len(review))
+    # Save the updated review list to the review file
+    data = pd.DataFrame(review)
+    data.to_csv("data/review_words.csv", index=False)
+    # Display the next flashcard
+    next_flashcard()
+
+
+# ---------- GUI setup ---------- #
 
 # Create the main window for the flashcard program
 window = Tk()
@@ -22,6 +71,9 @@ flashcard_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "ital
 flashcard_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.config(bg="#F5F5DC", highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
+
+# Set a timer to flip the flashcard after 4.5 seconds
+card_view_time = window.after(4500, func=flip_flashcard)
 
 # Create buttons for wrong and correct answers
 wrong_button = Button(image=wrong_image, highlightthickness=0, command=next_flashcard)
