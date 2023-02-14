bc8d93ff0a9ec3c9db000a10c5cbfb95f054dc81
Gijs Tempelman <gijstempelman@gmail.com>
2023-02-14 11:01:41 +0000

    Load packages and necessary images

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-BACKGROUND_COLOR = "#B1DDC6"
+# Import the necessary libraries
+from tkinter import *
+import pandas as pd
+import random
 
+# Load the images for the front, back, wrong and correct cards
+front_image = PhotoImage(file="images/front.png")
+back_image = PhotoImage(file="images/back.png")
+wrong_image = PhotoImage(file="images/wrong.png")
+correct_image = PhotoImage(file="images/right.png")
